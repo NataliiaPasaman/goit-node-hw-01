fbc69ede6504f429c1ee76a012528b677a45a20c
NataliiaPasaman <natapasaman@gmail.com>
2023-01-13 20:40:08 +0200

    task 2

---
+++ db/contacts.js
@@ -0,0 +1,75 @@
+const fs = require('fs').promises;
+const path = require('path');
+const uuid = require('uuid');
+
+const contactsPath = path.join(__dirname, 'contacts.json');
+
+const listContacts = async() => {
+    try {
+        const contacts = await fs.readFile(contactsPath);
+        const contactsParsed = JSON.parse(contacts);
+        console.log('contactsParsed', contactsParsed);
+        return contactsParsed;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const getContactById = async(contactId) => {
+    try {
+        const allContacts = await listContacts();
+        const findedContact = allContacts.find(({ id }) => Number(id) === contactId);
+        
+        if (!findedContact) return null;
+        console.log('findedContact-', findedContact);
+        
+        return findedContact;
+    } catch (error) {
+        console.log(error.message);
+    }
+  }
+
+const addContact = async(name, email, phone) => {
+    const newContact = {
+        id: uuid.v4(),
+        name,
+        email,
+        phone,
+    };
+
+    try {
+        const allContacts = await listContacts();
+        allContacts.push(newContact);
+
+        const newContacts = await fs.writeFile(contactsPath, JSON.stringify(allContacts));
+        return newContacts;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+const removeContact = async(contactId) => {
+    try {
+        const allContacts = await listContacts();
+        const index = allContacts.findIndex(({ id }) => Number(id) === contactId);
+
+        if (index === -1) {
+           console.log(`Contacts with index ${index} was not found`);
+           return;
+        }
+
+        const deleteContact = allContacts.splice(index, 1);
+        console.log('deleteContact-', deleteContact);
+        await fs.writeFile(contactsPath, JSON.stringify(allContacts));
+        return deleteContact;
+    } catch (error) {
+        console.log(error.message);
+    }
+  }
+
+  module.exports = {
+    listContacts,
+    getContactById,
+    addContact,
+    removeContact,
+  }
